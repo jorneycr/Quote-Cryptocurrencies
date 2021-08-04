5e30ddcab1eb996e9d4c60afd1e957f70b4bec08
jorneylm <jorney95@live.com>
2021-08-04 11:24:27 -0600

    commit

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import styled from '@emotion/styled';
+import Error from './Error';
 import useMoneda from '../hooks/useMoneda';
 import useCriptomoneda from '../hooks/useCriptomoneda';
 import axios from 'axios';
@@ -21,10 +22,11 @@ const Boton = styled.input`
     }
 `;
 
-const Formulario = () => {
+const Formulario = ({guardarMoneda, guardarCriptomoneda}) => {
 
     //state del listado de crytomonedas
     const [ listacrypto, guardarCriptomonedas] = useState([]);
+    const [error, guardarError] = useState(false);
 
     const MONEDAS =[
         {codigo: 'USD', nombre: 'Dolar USA'},
@@ -49,8 +51,26 @@ const Formulario = () => {
         consultarAPI();
     }, [])
 
+    const cotizarMoneda = e => {
+        e.preventDefault();
+
+        //valida si los campos estan llenos
+        if(moneda ==='' || criptomoneda === ''){
+            guardarError(true);
+            return;
+        }
+
+        //pasar los datos al componente principal
+        guardarError(false);
+        guardarMoneda(moneda);
+        guardarCriptomoneda(criptomoneda);
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={cotizarMoneda}
+        >
+            {error? <Error mensaje="Todos los campos son obligatorios"/>:null}
             <SelectMonedas/>
             <SelectCripto/>
             <Boton 
